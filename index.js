974454fdd37bf6ba6cea2f6d42835ecc97428094
Grace Han <ohlookitsgrace@gmail.com>
2018-10-17 00:31:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ console.log('hello')
 document.querySelector('#main').remove()
 
 /* Create your new element here and assign it to newHeader */
-let newHeader = document.createElement('h1')
+const newHeader = document.createElement('h1')
 newHeader.id = "victory"
 newHeader.innerText = "Grace is the champion!"
 
 document.body.appendChild(newHeader)
+const newHeader = document.querySelector('#victory')
