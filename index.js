113163e7315826bbc08410473983255408af2596
Grace Han <ohlookitsgrace@gmail.com>
2018-10-17 00:21:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 /* Enter the code to remove the main node element under this comment */
+console.log('hello')
+
+main.remove()
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+let newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.innerText = "Grace is the champion!"
+
+document.body.appendChild(newHeader)
